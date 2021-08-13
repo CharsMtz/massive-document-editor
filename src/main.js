7b64abc5257a22732ea341c7400444b9b5f16483
FernandoDavid <prepa.2001@gmail.com>
2021-08-13 07:45:46 -0500

    App working without design

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ const Pizzip = require('pizzip');
 const DocxTemplater = require('docxtemplater');
 const fs = require('fs');
 
+const homeDir = require('os').homedir();
+const desktopDir = `${homeDir}/Desktop/Documentos_Generados`;
 
 let window
 
@@ -27,17 +29,17 @@ function createWindow(){
 
 // Document generator functions
 function readExcel(){
-    const libro = xlsx.readFile('src/Docs/input/datos.xlsx');
+    const libro = xlsx.readFile('src/Docs/datos.xlsx');
     const hojasExcel = libro.SheetNames;
     const hoja = hojasExcel[0];
     const data = xlsx.utils.sheet_to_json(libro.Sheets[hoja]);
     return data;
 }
 
-function generateDocs(){
+async function generateDocs(){
     const data = readExcel();
     for(let i=0; i<data.length;i++){
-        var content = fs.readFileSync(path.resolve(__dirname, 'Docs/input/plantilla.docx'), 'binary');
+        var content = fs.readFileSync(path.resolve(__dirname, 'Docs/plantilla.docx'), 'binary');
         const zip = new Pizzip(content);
         const doc = new DocxTemplater(zip);
 
@@ -50,8 +52,30 @@ function generateDocs(){
             throw e;
         }
         var buf = doc.getZip().generate({ type: 'nodebuffer' });
-        fs.writeFileSync(`src/Docs/output/${i+1}.docx`, buf);
+        fs.writeFileSync(`${desktopDir}/${i+1}.docx`, buf);
     }
+}
+
+async function copyDocs(dataFilePath, templateFilePath){
+    fs.copyFile(templateFilePath, 'src/Docs/plantilla.docx', (err) => {
+        if (err) throw err;
+        console.log('template was copied');
+    });
+    fs.copyFile(dataFilePath, 'src/Docs/datos.xlsx', (err) => {
+        if (err) throw err;
+        console.log('excel was copied');
+    });
+}
+
+async function createDirectory(){
+    console.log(desktopDir);
+    fs.mkdir(desktopDir, { recursive: true }, function(err) {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log("New directory successfully created.")
+        }
+      })
 }
 
 
@@ -59,5 +83,7 @@ function generateDocs(){
 
 module.exports = {
     createWindow,
-    generateDocs
+    generateDocs,
+    copyDocs,
+    createDirectory
 }
--- src/ui/app.js
+++ src/ui/app.js
@@ -9,17 +9,13 @@ const templateFileInput = document.getElementById('filePlantilla');
 
 
 
-generateForm.addEventListener('submit', (e) =>{
-    e.preventDefault();
+generateForm.addEventListener('submit', () =>{
     const dataFilePath = dataFileInput.files[0].path;
-    const templateFilePath = dataFileInput.files[0].path;
-    fs.copyFile(templateFilePath, 'src/Docs/input/plantilla.docx', (err) => {
-        if (err) throw err;
-        console.log('doc was copied to destination.txt');
-    });
-    fs.copyFile(dataFilePath, 'src/Docs/input/datos.xlsx', (err) => {
-        if (err) throw err;
-        console.log('excel was copied to destination.txt');
-    });
-    main.generateDocs();
+    const templateFilePath = templateFileInput.files[0].path;
+
+    var copyDocs = main.copyDocs(dataFilePath, templateFilePath).then(() => {
+        var createDirectory = main.createDirectory().then(() => {
+            main.generateDocs();
+        })
+    })
 })
